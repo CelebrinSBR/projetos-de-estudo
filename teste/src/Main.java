b0fb0df94dce5395ea0881e8be30e1b970254966
Luiz Fernando Miguel Celebrin dos Santos <luizfernandoteen@gmail.com>
2025-07-22 16:18:32 -0300

    Atualizacao automatica

--- teste/src/Main.java
+++ teste/src/Main.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         {
 
-            System.out.println("Escreva a porra de um número");
+            System.out.println("Escreva a pindaiba de um número");
             int numero = sc.nextInt();
-            System.out.printf("Essa é a tabuada do seu numero de merda: %d de 1 a 10 \n",numero);
+            System.out.printf("Essa é a tabuada do seu numero de flores: %d de 1 a 10 \n",numero);
             for (int i = 1; i <= 10; i++){
                 System.out.println(numero+ " X " + i + " = " + (numero * i));
             }
